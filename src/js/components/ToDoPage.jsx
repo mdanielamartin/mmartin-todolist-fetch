88587a3e891b7f58272573cbf13d1eb757a0ccd8
Maria Daniela Martin <m.danielamartin@proton.me>
2025-01-03 21:07:43 -0400

    added useEffect functionality

--- src/js/components/ToDoPage.jsx
+++ src/js/components/ToDoPage.jsx
@@ -1,49 +1,69 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 
-//create your first component
 const ToDoPage = () => {
 
     const [list, setList] = useState([]);
     const [task, setTask] = useState('');
-    const [user, setUser] = useState({ 'name': '', 'id': null });
-    const [login, setlogin] = useState(false)
+    const [user, setUser] = useState('sampleUserJS');
     const [hover, setHover] = useState(null);
+    const login = useRef(true);
+
 
     const createUser = async () => {
-        if (user.name.length < 2) {
+        if (user.length < 2) {
             alert('Username is too short')
             return
         }
         try {
-            const response = await fetch(`https://playground.4geeks.com/todo/users/${user.name}`, {
+            const res = await fetch(`https://playground.4geeks.com/todo/users/${user}`, {
                 method: 'POST'
             })
-            if (!response.ok) {
-                throw new Error(response.statusText)
+            const registration = await res.json()
+            if (!res.ok) {
+                throw new Error(registration.detail)
             }
-            const registration = response.json();
-            setUser(prev => { return { ...prev, id: registration.id } })
-            setlogin(prev => !prev)
-            alert(`Username ${user.name} registered correctly, you may begin adding tasks`)
+            alert(`Username ${user} registered correctly, you may begin adding tasks`)
+            await updateRender();
         } catch (error) {
-            console.error('Problem registering user, server responded with:', error)
+            alert(error)
+            console.error('Problem registering user, server responded with:', error);
+            throw error;
         }
+    }
+
+    const logIn = async () => {
+        if (user.length < 2) {
+            alert('Username is too short')
+            return
+        }
+        try {
+            const response = await fetch(`https://playground.4geeks.com/todo/users/${user}`)
+            const todoList = await response.json();
+            if (!response.ok) {
+                throw new Error(todoList.detail)
+            }
+            alert('Login successful')
+            setList(todoList.todos)
+        } catch (error) {
+            alert(error)
+            console.error('Problem login, server responded with:', error)
+        }
+
     }
 
     const logOut = async () => {
         try {
-            const response = await fetch(`https://playground.4geeks.com/todo/users/${user.name}`, {
+            const response = await fetch(`https://playground.4geeks.com/todo/users/${user}`, {
                 method: 'DELETE'
             })
             if (!response.ok) {
                 throw new Error(response.statusText)
             }
-            setUser({ 'name': '', 'id': null })
+            setUser('')
             alert(`Logout successful and task list cleared`)
-            setlogin(prev => !prev)
             setList([])
         } catch (error) {
             console.error('Problem login out, server responded with:', error)
@@ -53,21 +73,23 @@ const ToDoPage = () => {
 
     const updateRender = async () => {
         try {
-            const response = await fetch(`https://playground.4geeks.com/todo/users/${user.name}`)
-            if (!response.ok) {
-                throw new Error(response.statusText)
-            }
+            const response = await fetch(`https://playground.4geeks.com/todo/users/${user}`)
             const todoList = await response.json();
+            if (!response.ok) {
+                throw new Error(todoList.detail)
+            }
             setList(todoList.todos)
         } catch (error) {
+            alert('Create an user or login first before adding tasks')
             console.error('Problem updating, server responded with:', error)
+            throw error;
         }
     }
 
     const addTask = async (event) => {
-        if (event.key === 'Enter') {
+        if (event.key === 'Enter' && task !== '') {
             try {
-                const response = await fetch(`https://playground.4geeks.com/todo/todos/${user.name}`, {
+                const response = await fetch(`https://playground.4geeks.com/todo/todos/${user}`, {
                     method: 'POST',
                     headers: { 'content-type': 'application/json' },
                     body: JSON.stringify({ 'label': task, 'is_done': false }),
@@ -75,10 +97,9 @@ const ToDoPage = () => {
                 if (!response.ok) {
                     throw new Error(response.statusText)
                 }
-                const taskData = await response.json();
                 setTask('')
+                await updateRender();
             } catch (error) {
-                alert('Problem adding task, server responded with:', error)
                 console.error('Problem adding task, server responded with:', error)
             }
         }
@@ -92,8 +113,8 @@ const ToDoPage = () => {
             if (!response.ok) {
                 throw new Error(response.statusText)
             }
+            await updateRender();
         } catch (error) {
-            alert('Problem deleting task, server responded with:', error)
             console.error('Problem deleting task, server responded with:', error)
         }
     }
@@ -108,30 +129,69 @@ const ToDoPage = () => {
             if (!response.ok) {
                 throw new Error(response.statusText)
             }
+            await updateRender();
         } catch (error) {
-            alert('Problem updating task, server responded with:', error)
             console.error('Problem updating task, server responded with:', error)
         }
     }
 
+    useEffect(() => {
+        const firstLogIn = async () => {
+            try {
+                const res = await fetch(`https://playground.4geeks.com/todo/users/${user}`, {
+                    method: 'POST'
+                })
+                if (!res.ok) {
+                    throw new Error(res.statusText)
+                }
+                await updateRender();
+            } catch {
+                console.error('User already exists, attempting to retrieve information')
+                try {
+                    await updateRender();
+                } catch {
+                    console.error('Creating user and updating render failed, problem with server')
+
+                }
+            }
+        }
+        if (login.current) {
+            firstLogIn();
+        }
+        login.current = false;
+    }, [])
+
     return (
         <div className="page">
             <div className="d-flex my-3 login align-items-center justify-content-around">
-                <input className="form-control fs-5 ms-3" type="text" placeholder="Type your username" disabled={login} value={user.name} onChange={(e) => { setUser(prev => { return { ...prev, 'name': e.target.value } }) }} />
+                <input className="form-control fs-5 ms-3" type="text" placeholder="Type your username" value={user} onChange={(e) => { setUser(e.target.value) }} />
                 <div className="d-flex mx-3 justify-content-between align-items-center w-100">
-                    <button type="button" className="btn btn-success fw-bold mx-2" disabled={login} onClick={createUser}>Login</button>
-                    <button type="button" className="btn btn-danger fw-bold mx-2" disabled={!login} onClick={logOut}>Logout & Clear Data</button>
+                    <button type="button" className="btn btn-success fw-bold mx-2" onClick={createUser}>Create User</button>
+                    <button type="button" className="btn btn-primary fw-bold mx-2" onClick={logIn}>Login User</button>
+                    <button type="button" className="btn btn-danger fw-bold mx-2" onClick={logOut}>Delete User</button>
                 </div>
             </div>
             <h1 className="title">To Do List</h1>
-            <input className="form-control py-3 input-bar b-0 my-4" type="text" placeholder="Type your next task..." disabled={!login} value={task} onKeyDown={(e) => addTask(e).then(updateRender)} onChange={(e) => { setTask(e.target.value) }} />
+            <input className="form-control py-3 input-bar b-0 my-4" type="text" placeholder="Type your next task..." disabled={user === '' ? true : false} value={task} onKeyDown={(e) => addTask(e).then(updateRender)} onChange={(e) => { setTask(e.target.value) }} />
             <div className="task-list">
                 <ul className="list-group list-group-flush list-items">
                     {list.map((item) => {
                         return (
-                            <li className="list-group-item align-middle d-flex justify-content-start" key={item.id} onMouseEnter={() => {
-                                setHover(item.id)
-                            }} onMouseLeave={() => { setHover(null) }}><input className="form-check-input me-5" type="checkbox" value="" id="flexCheckDefault" onClick={() => markedDone(item).then(updateRender)} /><div className={`task ${item.is_done ? 'task-done' : ''}`}>{item.label}</div> <FontAwesomeIcon icon={faTrash} className={`delete ${hover === item.id ? 'delete-show' : ''}`} onClick={() => deleteTask(item.id).then(updateRender)} /></li>
+                            <li className="list-group-item align-middle d-flex justify-content-start"
+                                key={item.id}
+                                onMouseEnter={() => {
+                                    setHover(item.id)
+                                }}
+                                onMouseLeave={() => { setHover(null) }}>
+                                <input className="form-check-input me-5" type="checkbox"
+                                    checked={item.is_done}
+                                    value=""
+                                    id="flexCheckDefault"
+                                    onChange={() => markedDone(item)} />
+                                <div className={`task ${item.is_done ? 'task-done' : ''}`}>
+                                    {item.label}</div>
+                                <FontAwesomeIcon icon={faTrash} className={`delete ${hover === item.id ? 'delete-show' : ''}`}
+                                    onClick={() => deleteTask(item.id)} /></li>
                         )
                     })}
                 </ul>
